a1028e415d7c093512805fb9b9a572cc420562e4
Tadiwa Blexta <youngtips23@gmail.com>
2025-12-02 12:16:17 +0200

    added auth endpoint

--- src/main/java/co/zw/blexta/checkmate/config/SecurityConfig.java
+++ src/main/java/co/zw/blexta/checkmate/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                                 
                                 .requestMatchers("/error").permitAll()
                                 
-                                .anyRequest().permitAll()
+                                .anyRequest().authenticated()
                 )
                 .formLogin(login -> login.disable())
                 .httpBasic(basic -> basic.disable())
